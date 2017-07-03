1c4ac03b0f4cf59966074efed63058f1f1a5640f
pmutua <pmutua@live.com>
2017-07-03 14:35:18 +0300

    *added test on spec file

--- lib/task.rb
+++ lib/task.rb
@@ -6,4 +6,16 @@ class Task
     define_method(:description) do
         @description
     end
+
+    define_singleton_method(:all) do
+      []
+    end
 end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new("wash the lion")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
